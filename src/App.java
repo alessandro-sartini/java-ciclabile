085adbb1a9624835769e4a71d102caaa1e926c9f
alessandro-sartini <ale.sartini.98@gmail.com>
2025-05-09 10:10:17 +0200

    REFINE: format array initialization and enhance constructor usage in App

--- src/App.java
+++ src/App.java
@@ -2,14 +2,19 @@ import com.ElencoInteri;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] numeri={1,2,3,4,5};
+        int[] numeri = { 1, 2, 3, 4, 5 };
 
-        ElencoInteri elencoInteri =new ElencoInteri();
+        ElencoInteri elencoInteri = new ElencoInteri(numeri);
         elencoInteri.addInt(7);
         while (elencoInteri.hasAncoraElementi()) {
             System.out.println(elencoInteri.getElementoSuccessivo());
         }
+        System.out.println("--------- costruttore vuoto!! ---------");
+        ElencoInteri elenco = new ElencoInteri();
+        elenco.addInt(1);
+        while (elenco.hasAncoraElementi()) {
+            System.out.println(elenco.getElementoSuccessivo());
+        }
 
-       
     }
 }
