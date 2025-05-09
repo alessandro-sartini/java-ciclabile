bde8f90ff5bd3eb4e27db669c392ee47adfc9f9f
alessandro-sartini <ale.sartini.98@gmail.com>
2025-05-09 09:57:24 +0200

    ADD: getter e setter

--- src/com/ElencoInteri.java
+++ src/com/ElencoInteri.java
@@ -2,15 +2,32 @@ package com;
 
 public class ElencoInteri {
 
-    public int[] arrayIterno;
-    public int nextIndex;
-    public ElencoInteri(){
-        this.arrayIterno=new int[0];
-    }
+    private int[] arrayIterno;
+    private int nextIndex;
 
     public ElencoInteri(int[] array) {
         this.arrayIterno = array;
-        this.nextIndex=0;
+        this.nextIndex = 0;
+    }
+
+    public int[] getArrayIterno() {
+        return this.arrayIterno;
+    }
+
+    public void setArrayIterno(int[] arrayIterno) {
+        this.arrayIterno = arrayIterno;
+    }
+
+    public int getNextIndex() {
+        return this.nextIndex;
+    }
+
+    public void setNextIndex(int nextIndex) {
+        this.nextIndex = nextIndex;
+    }
+
+    public ElencoInteri() {
+        this.arrayIterno = new int[0];
     }
 
     public int getElementoSuccessivo() {
@@ -20,25 +37,23 @@ public class ElencoInteri {
 
     }
 
-    public boolean hasAncoraElementi(){
-        boolean hasNext=true;
+    public boolean hasAncoraElementi() {
+        boolean hasNext = true;
 
         if (nextIndex >= arrayIterno.length) {
-            hasNext=false;
+            hasNext = false;
         }
 
         return hasNext;
     }
 
-    public void addInt(int newInt){
-        int[] updated=new int[arrayIterno.length+1];
-        for(int i=0;i<this.arrayIterno.length;i++){
-            updated[i]=this.arrayIterno[i];
+    public void addInt(int newInt) {
+        int[] updated = new int[arrayIterno.length + 1];
+        for (int i = 0; i < this.arrayIterno.length; i++) {
+            updated[i] = this.arrayIterno[i];
         }
-        updated[updated.length-1]=newInt;
-        this.arrayIterno=updated;
+        updated[updated.length - 1] = newInt;
+        this.arrayIterno = updated;
     }
-       
-        
 
 }
